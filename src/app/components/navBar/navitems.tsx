045d86062a476148623db9c4581433f8ca286493
hackhim18 <triphimanshu18@gmail.com>
2021-06-29 16:54:31 +0530

    c5

--- src/app/components/navBar/navitems.tsx
+++ src/app/components/navBar/navitems.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import styled, { css } from "styled-components";
 import tw from "twin.macro";
+import { slide as Menu } from "react-burger-menu";
+import { useMediaQuery } from "react-responsive";
+import { SCREENS } from "../responsive";
+import menuStyles from "./menuStyles";
 
 const ListContainer = styled.ul`
   ${tw`
@@ -23,12 +27,25 @@ const NavItem = styled.li<{ menu?: any }>`
     ease-in-out
     hover:text-gray-700
   `};
- 
+  ${({ menu }) =>
+    menu &&
+    css`
+      ${tw`
+      text-white
+      text-xl
+      mb-3
+      focus:text-white
+    `};
+    `};
 `;
 
 export function NavItems() {
+  const isMobile = useMediaQuery({ maxWidth: SCREENS.sm });
+
+  if (isMobile)
     return (
-     <ListContainer>
+      <Menu right styles={menuStyles}>
+        <ListContainer>
           <NavItem menu>
             <a href="#">Home</a>
           </NavItem>
@@ -42,7 +59,7 @@ export function NavItems() {
             <a href="#">Contact Us</a>
           </NavItem>
         </ListContainer>
-     
+      </Menu>
     );
 
   return (
