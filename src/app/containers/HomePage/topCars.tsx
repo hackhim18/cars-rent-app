2a7c850a7190d21d1547f8660cbadcbfcaf77a02
hackhim18 <triphimanshu18@gmail.com>
2021-07-04 22:15:54 +0530

    c13

---
+++ src/app/containers/HomePage/topCars.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+import tw from 'twin.macro';
+
+const TopCarsContainer= styled.div`
+${tw`
+
+
+`};
+`;
+
+
+export function TopCars() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+
